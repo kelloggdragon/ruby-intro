3f8301ebd20b30e312d7baacf6f13658444d0f22
Dragon Chan <dragon.chan@kellogg.northwestern.edu>
2026-01-29 02:34:56 +0000

    Did hour 2 of class

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,29 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+favorite_foods = ["tacos","pizza","ice cream"]
+# p favorite_foods
+
+numbers = [2,5,8,11]
+
+# p numbers
+
+mixed_stuff = ["paper",23,true]
+# p mixed_stuff
 
 # Accessing the array
 
+p favorite_foods[0]
+puts favorite_foods[1]
+p favorite_foods[3]
+
 # Add to the array
+
+groceries = favorite_foods + ["apple juice", "more tacos"]
+more_foods = favorite_foods << "avocados"
+p more_foods
+favorite_foods.push "cheese"
+p favorite_foods
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,35 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
-
+profile = {
+  "name" => "Ben",
+  "location" => "Chicago",
+  "status" => "Teaching"
+}
 # Accessing data from the hash
 
+puts profile
+name = profile["name"]
+puts "Hello, #{name}"
+
+profile["age"] = 44
+
+puts profile
+
 # More Complex Hashes
+# 
+profile["location"] = {"city" => "Chicago", "state" => "IL"}
+puts profile
+
+puts profile["location"]["state"]
+
+timeline = [
+  {"status" => "Teaching", "occured_at" => "8:30pm"},
+  {"status" => "Reading", "occured_at" => "10:30pm"},
+  {"status" => "Sleeping", "occured_at" => "12:30am"}
+]
+
+profile["timeline"] = timeline
+puts profile
+puts "Right now:"
+puts profile["timeline"][0]["status"]
